9e939b190822df01356ef74550259b0da4d59a3f
Arnas Vaicekauskas <arnasvaic7@gmail.com>
2025-10-09 18:55:07 +0300

    commit

--- task-2/src/main.cpp
+++ task-2/src/main.cpp
@@ -27,8 +27,8 @@ constexpr int total_points = N + 1;
 constexpr double h = 1 / static_cast<double>(N);
 constexpr double h2 = h * h;
 
-constexpr double T = 1;
-constexpr double tau = 0.0001;
+constexpr double T = 40;
+constexpr double tau = 0.001;
 constexpr int total_time_steps = static_cast<int>(T / tau);
 
 // constructing the f(x, t) so that it would fit the exact solution:
